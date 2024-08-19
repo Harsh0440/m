72568026566982c837610d66097ade8b61593d0b
Harsh0440 <145108922+Harsh0440@users.noreply.github.com>
2024-08-19 09:51:00 +0530

    routes

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Projects from './pages/Projects'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/projects' element={<Projects/>} />
+      </Routes>
+    
+    </BrowserRouter>
+  )
+}
